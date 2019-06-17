6d8b12930f03bd2ecbbd471849a880e9d5dd6a99
Murat Özcan <38462764+Murat-Ozcan@users.noreply.github.com>
2019-06-17 23:05:50 +0300

    Add project files.

---
+++ LogApi/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LogLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public ValuesController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.Log(new LogEntry(LoggingEventType.Information, "Deneme Murat", new Exception("Sallamasyon Hata")));
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LogLayer/SerilogService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Serilog.Events;
+
+namespace LogLayer
+{
+    public class SerilogService : ILogger
+    {
+        private readonly Serilog.ILogger _logger;
+
+        public SerilogService(Serilog.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log(LogEntry entry)
+        {
+            switch (entry.Severity)
+            {
+                case LoggingEventType.Information when IsEnabledFor(LoggingEventType.Information):
+                    _logger.Information(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Debug when IsEnabledFor(LoggingEventType.Debug):
+                    _logger.Debug(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Warning when IsEnabledFor(LoggingEventType.Warning):
+                    _logger.Warning(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Error when IsEnabledFor(LoggingEventType.Error):
+                    _logger.Error(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Fatal when IsEnabledFor(LoggingEventType.Fatal):
+                    _logger.Fatal(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Trace when IsEnabledFor(LoggingEventType.Trace):
+                    _logger.Verbose(entry.Exception, entry.Message);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public bool IsEnabledFor(LoggingEventType severityType)
+        {
+            switch (severityType)
+            {
+                case LoggingEventType.Information:
+                    return _logger.IsEnabled(LogEventLevel.Information);
+                case LoggingEventType.Debug:
+                    return _logger.IsEnabled(LogEventLevel.Debug);
+                case LoggingEventType.Warning:
+                    return _logger.IsEnabled(LogEventLevel.Warning);
+                case LoggingEventType.Error:
+                    return _logger.IsEnabled(LogEventLevel.Error);
+                case LoggingEventType.Fatal:
+                    return _logger.IsEnabled(LogEventLevel.Fatal);
+                case LoggingEventType.Trace:
+                    return _logger.IsEnabled(LogEventLevel.Verbose);
+                default:
+                    return false;
+            }
+        }
+    }
+}
